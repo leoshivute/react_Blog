ede46aa8a100cec4c7c48ccb4366abfaf32c79ad
Shivute <Leonard.Shivute@standardbank.com.na>
2019-09-18 12:01:22 +0200

    First commit

---
+++ src/build/precache-manifest.d480fcbe094f2e75565045498b647df8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f921f8f63297055d40580d657e810cb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43a88e16b9e14e860191",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "377ad4a661bc83a76eca",
+    "url": "/static/js/2.e31f970e.chunk.js"
+  },
+  {
+    "revision": "43a88e16b9e14e860191",
+    "url": "/static/js/main.92e10e2b.chunk.js"
+  },
+  {
+    "revision": "16f57bc6dde3f2278832",
+    "url": "/static/js/runtime~main.82f07dd6.js"
+  }
+]);
